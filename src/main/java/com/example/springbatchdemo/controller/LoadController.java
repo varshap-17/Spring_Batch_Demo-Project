00f5f01b7a565e218d0c3cb5c14be3d7e38bf06a
varshap-17 <varshap630@gmail.com>
2022-11-14 14:06:09 +0530

    Used to load the rest api with job launcher

---
+++ src/main/java/com/example/springbatchdemo/controller/LoadController.java
@@ -0,0 +1,36 @@
+package com.example.springbatchdemo.controller;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/load")
+public class LoadController {
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    Job job;
+    @GetMapping
+    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        Map<String,JobParameter> maps=new HashMap<>();
+        maps.put("time",new JobParameter(System.currentTimeMillis()));
+        JobParameters parameter= new JobParameters(maps);
+        JobExecution jobExecution =jobLauncher.run(job,parameter);
+        System.out.println("JobExecution:"+jobExecution.getStatus());
+        System.out.println("Batch is running");
+        while (jobExecution.isRunning()){
+            System.out.println("...");
+        }
+        return jobExecution.getStatus();
+    }
+}
